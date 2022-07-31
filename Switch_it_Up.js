2ce1f3dafd2d489c91ba6926a7fa3d8f0efabf3e
Juventtini <juventtinec@gmail.com>
2022-07-31 17:01:14 +0300

    feat: add solve to easy task KYI 7-8 base of js

---
+++ Switch_it_Up.js
@@ -0,0 +1,12 @@
+/* When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement. */
+
+function switchItUp(number) {
+    let arr = ["zero","one", 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+return arr[number]
+}
